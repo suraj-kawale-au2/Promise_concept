d527c4d17a940addd345647d2a419a7660e113ac
surajk202 <kawalesuraj76@gmail.com>
2019-09-23 20:52:49 +0530

    testcase corrected

--- function.test.js
+++ function.test.js
@@ -5,7 +5,7 @@ test('should add 2+2 = 4 ', () => {
 });
 
 //Promise
-test('User fetched name should be',()=>{
+test('api calls should be 2 and should give valid sum of followers and following',()=>{
     sums = [576,21657,21014,8220,9368,508,93,1249,106,715,633,2513,1105,89]
     expect.assertions(2);
     return functions.fetchSum().then(sum=>{
